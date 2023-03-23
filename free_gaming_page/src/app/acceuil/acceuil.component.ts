f81eadf28fce482f73fb14a659dcc3ccc035fc71
Thomas-FAGOT <thomasfagot@ynov.com>
2023-03-23 22:42:32 +0100

    Dev page Acceuil

--- free_gaming_page/src/app/acceuil/acceuil.component.ts
+++ free_gaming_page/src/app/acceuil/acceuil.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
-  styleUrls: ['./acceuil.component.scss']
+  styleUrls: ['./acceuil.component.scss'],
 })
 export class AcceuilComponent implements OnInit {
 
