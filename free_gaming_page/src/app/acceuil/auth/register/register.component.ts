06585ef3710e6a7b8cf609de037ca987ba97f904
MaximeLombard <112693934+MaximeLombard@users.noreply.github.com>
2023-03-24 10:33:10 +0100

    Register and Login Component

--- free_gaming_page/src/app/acceuil/auth/register/register.component.ts
+++ free_gaming_page/src/app/acceuil/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import {
   AbstractControl,
   FormControl,
@@ -17,9 +17,56 @@ import { EmailService } from 'src/app/services/email/email.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    hide = true;
+  public form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+  public _loginError400 = new BehaviorSubject<boolean>(false);
+  public _loginError401 = new BehaviorSubject<boolean>(false);
+
+    public errors = {
+    email: {
+      error: false,
+      message: `Format d'email incorrect`
+    },
+    password: {
+      error: false,
+      message: 'Le mot de passe est obligatoire'
+    }
+  };
+
+  constructor(private dialogRef: MatDialogRef<RegisterComponent>,
+    private emailService: EmailService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+      emailValidator(c: AbstractControl) {
+    return this.emailService.isEmailValid(c.value)
+      ? null
+      : {
+          email: {
+            valid: false
+          }
+        };
+  }
+
+  handlePasswordError(value: string) {
+    const isEmptyString = value.length === 0;
+    if (isEmptyString) {
+      this.errors.password.error = true;
+      return;
+    }
+    this.errors.password.error = false;
+  }
+
+  handleEmailError(value: string) {
+    this.errors.email = this.emailService.getEmailError(
+      value,
+      this.errors.email
+    );
   }
 
 }
